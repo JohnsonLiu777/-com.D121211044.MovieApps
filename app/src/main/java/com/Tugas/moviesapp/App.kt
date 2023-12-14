5a6946c8bba6e315c8f83ffea230bf0fa579e5aa
Johnson Liu <120615297+JohnsonLiu777@users.noreply.github.com>
2023-12-14 15:42:22 +0800

    perbaikan

--- app/src/main/java/com/Tugas/moviesapp/App.kt
+++ app/src/main/java/com/Tugas/moviesapp/App.kt
@@ -3,8 +3,6 @@ package com.Tugas.moviesapp
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-/**
- * @author Android Devs Academy (Ahmed Guedmioui)
- */
+
 @HiltAndroidApp
 class App: Application()
